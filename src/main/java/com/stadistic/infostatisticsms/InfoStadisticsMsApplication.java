821f438d0cf6867d8e6a9ae5312041ad4cf6ef5b
German <geralvaredo@gmail.com>
2024-05-01 15:30:02 -0300

    estructura

---
+++ src/main/java/com/stadistic/infostatisticsms/InfoStadisticsMsApplication.java
@@ -0,0 +1,12 @@
+package com.stadistic.infostatisticsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoStadisticsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoStadisticsMsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stadistic/infostatisticsms/InfoStadisticsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stadistic.infostatisticsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoStadisticsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
